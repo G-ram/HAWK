bb1b96f92fab275fef49effeafcd8dbaf9cd0088
fatsmcgee <ethan.a.benjamin@gmail.com>
2015-11-14 16:01:36 -0500

    Have main pretty print test.hawk

--- compiler/main.ml
+++ compiler/main.ml
@@ -1,4 +1,7 @@
 open Ast
-
-let lexbuf = Lexing.from_channel (open_in Sys.argv.(1))
-let p = Parser.program Scanner.token lexbuf
+open Prettyprint
+	
+let lexbuf = Lexing.from_channel (open_in "test.hawk") in
+let p = Parser.program Scanner.token lexbuf in
+print_endline (Prettyprint.string_of_program p);;
+	
