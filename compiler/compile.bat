e0ca747fadfa48a4eb8d917b6d9de98b34f65ba4
fatsmcgee <ethan.a.benjamin@gmail.com>
2015-11-14 16:38:20 -0500

    Add a test file that just pretty prints test.hawk

--- compiler/compile.bat
+++ compiler/compile.bat
@@ -6,5 +6,7 @@ ocamlc -c parser.mli
 ocamlc -c scanner.ml 
 ocamlc -c parser.ml 
 ocamlc -c main.ml 
+ocamlc -c run_test_hawk.ml
+ocamlc -o runtesthawk.exe parser.cmo scanner.cmo prettyprint.cmo run_test_hawk.cmo
 ocamlc -o main.exe parser.cmo scanner.cmo prettyprint.cmo main.cmo 
 
