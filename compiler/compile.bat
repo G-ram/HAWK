b3c681e2642e3e2c94e4b225e20a077b7ee0c6c0
G-Ram <ggobieski@yahoo.com>
2015-11-14 16:21:35 -0500

    compiles

--- compiler/compile.bat
+++ compiler/compile.bat
@@ -1,10 +1,10 @@
-ocamllex scanner.mll 
-ocamlyacc parser.mly 
-ocamlc -c ast.mli 
+ocamllex scanner.mll
+ocamlyacc parser.mly
+ocamlc -c ast.mli
 ocamlc -c prettyprint.ml
-ocamlc -c parser.mli 
-ocamlc -c scanner.ml 
-ocamlc -c parser.ml 
-ocamlc -c main.ml 
-ocamlc -o main.exe parser.cmo scanner.cmo prettyprint.cmo main.cmo 
-
+ocamlc -c sast.ml
+ocamlc -c parser.mli
+ocamlc -c scanner.ml
+ocamlc -c parser.ml
+ocamlc -c main.ml
+ocamlc -o main.exe parser.cmo scanner.cmo prettyprint.cmo sast.cmo main.cmo
