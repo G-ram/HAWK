79dc6072dc1f3225855af78f4625fd69e69c2d24
G-Ram <ggobieski@yahoo.com>
2015-11-29 15:34:58 -0500

    testing codegen

--- compiler/main.ml
+++ compiler/main.ml
@@ -4,4 +4,4 @@ open Prettyprint
 let lexbuf = Lexing.from_channel (open_in Sys.argv.(1));;
 let p = Parser.program Scanner.token lexbuf;;
 let sp = Semantics.check_program p;;
-print_endline (Prettyprint.string_of_program p);;
+print_endline (Generator.string_of_program sp);;
