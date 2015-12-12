140963246c7dde54e4df52c93cdfc44a78e7a4a7
G-Ram <ggobieski@yahoo.com>
2015-12-12 17:10:20 -0500

    added CSS pattern matching

--- compiler/_hawk_lib/_HAWKCSSMatcher.java
+++ compiler/_hawk_lib/_HAWKCSSMatcher.java
@@ -4,7 +4,7 @@ public class _HAWKCSSMatcher{
     public _HAWKCSSMatcher(String aInput){
         input = aInput;
     }
-    public org.jsoup.nodes.Element[] _match(String pat){
+    public org.jsoup.select.Elements _match(String pat){
 
         org.jsoup.nodes.Document doc = org.jsoup.Jsoup.parse(input);
         return doc.select(pat);
