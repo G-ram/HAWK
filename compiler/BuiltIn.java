02a9fb7a66545ad1721e9f507e34748d0c223f83
G-Ram <ggobieski@yahoo.com>
2015-12-15 15:48:39 -0500

    added exists and length

--- compiler/BuiltIn.java
+++ compiler/BuiltIn.java
@@ -10,3 +10,15 @@ private static Boolean _checkIf(Double d){
 private static Boolean _checkIf(Boolean b){
   return b;
 }
+private static <T> int length(_HAWKTable<T> t){
+  return t.getLength();
+}
+private static <T> int exists(T e){
+  if(e == null){
+    return 0;
+  }
+  return 1;
+}
+private static <T> java.util.Set<String> keys(_HAWKTable<T> t){
+  return t.getKeys();
+}
--- compiler/_hawk_lib/_HAWKTable.java
+++ compiler/_hawk_lib/_HAWKTable.java
@@ -1,7 +1,7 @@
 package _hawk_lib;
 
 public class _HAWKTable<T>{
-	
+
 	private java.util.Hashtable<String,T> storage;
 
 	public _HAWKTable(){
@@ -12,43 +12,46 @@ public class _HAWKTable<T>{
 	private static String intToString(int i){
 		return i + "";
 	}
-	
-    public T getIntIndex(int i){
-        return storage.get(intToString(i));
-    }
 
-    public T getStringIndex(String s){
-        return storage.get(s);
-    }
+  public T getIntIndex(int i){
+      return storage.get(intToString(i));
+  }
 
-    public _HAWKTable setIntIndexChained(int i, T t){
-        storage.put(intToString(i),t);
-        return this;
-    }
+  public T getStringIndex(String s){
+      return storage.get(s);
+  }
 
-    public _HAWKTable setStringIndexChained(String s, T t){
-        storage.put(s,t);
-        return this;
-    }
+  public _HAWKTable setIntIndexChained(int i, T t){
+      storage.put(intToString(i),t);
+      return this;
+  }
 
-    public void setIntIndex(int i, T t){
-        storage.put(intToString(i),t);
-    }
+  public _HAWKTable setStringIndexChained(String s, T t){
+      storage.put(s,t);
+      return this;
+  }
 
-    public void setStringIndex(String s, T t){
-        storage.put(s,t);
-    }
-	
+  public void setIntIndex(int i, T t){
+      storage.put(intToString(i),t);
+  }
+
+  public void setStringIndex(String s, T t){
+      storage.put(s,t);
+  }
+
 	public java.util.Set<String> getKeys(){
 		return storage.keySet();
 	}
 
-    public String toString(){
-        String temp = "";
-        for( java.util.Map.Entry<String, T> entry : storage.entrySet() ){
-            temp += entry.toString();
-        } 
-        return temp;
-    }
-}
+	public int getLength(){
+		return storage.size();
+	}
 
+  public String toString(){
+      String temp = "";
+      for( java.util.Map.Entry<String, T> entry : storage.entrySet() ){
+          temp += entry.toString();
+      }
+      return temp;
+  }
+}
