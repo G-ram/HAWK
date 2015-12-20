a165e4b08f912dd8b823d69c18a3f016848ac2e5
G-Ram <ggobieski@yahoo.com>
2015-12-19 21:20:54 -0500

    small error with demo 3 fix

--- compiler/demos/demo3.sh
+++ compiler/demos/demo3.sh
@@ -1,3 +1,3 @@
 args="$*"
-q=${args/ /'%20'}
+q=${args// /'%20'}
 ./prog ./demos/demo3.hk > Program.java && javac Program.java && java -cp ./_hawk_lib/jsoup.jar: Program http://www.wunderground.com/cgi-bin/findweather/getForecast?query=$q
