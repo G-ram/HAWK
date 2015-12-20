68f047269630191df6175adfe694e3958f3eb32d
G-Ram <ggobieski@yahoo.com>
2015-12-19 22:11:02 -0500

    added line counter

---
+++ compiler/demos/demo1.sh
@@ -0,0 +1,1 @@
+./prog ./demos/demo1.hk > Program.java && javac Program.java && java -cp ./_hawk_lib/jsoup.jar: Program ./demos/regexdemo.txt
---
+++ compiler/demos/demo2.sh
@@ -0,0 +1,1 @@
+./prog ./demos/demo2.hk > Program.java && javac Program.java && java -cp ./_hawk_lib/jsoup.jar: Program http://www.reddit.com
